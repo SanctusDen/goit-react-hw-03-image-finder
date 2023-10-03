9c62567ca2331044bae77d932febd2a490ee91ee
Dionisii <121027564+SanctusDen@users.noreply.github.com>
2023-10-03 22:25:48 +0200

    skeleton

---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Component } from "react";
+
+export class ImageGalleryItem extends Component {
+    render() {
+        return (
+            <li class="gallery-item">
+                <img src="" alt="" />
+            </li>
+        )
+    }
+};
